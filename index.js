aef23e6966ba96a53d3d46a92f2c5ec0c0876347
Nicolas Cuillery <nicolas.cuillery@gmail.com>
2015-03-17 17:42:55 +0100

    Support --name options to filter spec files

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ module.exports = function (options) {
     options = options || {};
     return through.obj(function (file, enc, cb) {
         options.rootDir = options.rootDir || file.path;
-        jest.runCLI({
-            config: options
-        }, options.rootDir, function (success) {
+
+        var cliOptions = {config: options};
+        for(var i = 0, j = process.argv.length; i < j; i++) {
+            if(process.argv[i].indexOf('--name=') === 0) {
+                var value = process.argv[i].substring('--name='.length);
+                cliOptions.testPathPattern = new RegExp(value);
+            }
+        }
+
+        jest.runCLI(cliOptions, options.rootDir, function (success) {
             if(!success) {
                 cb(new gutil.PluginError('gulp-jest', { message: "Tests Failed" }));
             } else {
